233f61348dcec9e787fb101c3d2d19815e221e49
DevDeVice <sejkop@gmail.com>
2023-09-28 20:41:19 +0200

    Initial commit

---
+++ Ocena Pracownicza/DataModels/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocena_Pracownicza.DataModels
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Evaluation> Evaluations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;database=mydbname;user=myusername;password=mypassword",
+                                    new MySqlServerVersion(new Version(8, 2, 4)));
+        }
+    }
+}
---
+++ Ocena Pracownicza/DataModels/Evaluation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocena_Pracownicza.DataModels
+{
+    public class Evaluation
+    {
+        public int EvaluationID { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public string EvaluatorName { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Comments { get; set; }
+    }
+}
