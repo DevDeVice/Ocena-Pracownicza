a712cbf136656690c69ea023f82ad970dcbf0fa6
DevDeVice <sejkop@gmail.com>
2024-04-01 22:35:38 +0200

    githubTest

--- Ocena Pracownicza/DataModels/AppDbContext.cs
+++ Ocena Pracownicza/DataModels/AppDbContext.cs
@@ -17,7 +17,7 @@ namespace Ocena_Pracownicza.DataModels
         public DbSet<EvaluationBiuroAnswer> EvaluationBiuroAnswers { get; set; }
         public DbSet<EvaluationProdukcjaAnswer> EvaluationProdukcjaAnswers { get; set; }
         public DbSet<Department> Department { get; set; }
-         
+          
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql("server=localhost;database=ocena_pracownicza;user=root;password=",
