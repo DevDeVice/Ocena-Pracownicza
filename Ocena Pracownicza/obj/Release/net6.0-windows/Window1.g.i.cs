59e1d63d153d8bf23070b96da0e95aafa3fbf8b2
DevDeVice <40960987+DevDeVice@users.noreply.github.com>
2024-04-08 15:18:42 +0200

    up

--- Ocena Pracownicza/obj/Release/net6.0-windows/Window1.g.i.cs
+++ Ocena Pracownicza/obj/Release/net6.0-windows/Window1.g.i.cs
@@ -46,13 +46,13 @@ namespace Ocena_Pracownicza {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Ocena Pracownicza;component/window1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Ocena Pracownicza;V1.0.0.0;component/window1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -62,7 +62,7 @@ namespace Ocena_Pracownicza {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
